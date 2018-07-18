6491e3f9c0243e426fd5dba546eab3f3c9889a2b
Shashi Kant <skant743@gmail.com>
2018-07-18 10:38:58 +0530

    created components

--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -3,11 +3,24 @@ import { NgModule } from '@angular/core';
 
 
 import { AppComponent } from './app.component';
+import { NavBarComponent } from './components/nav-bar/nav-bar.component';
+import { LoginComponent } from './components/login/login.component';
+import { SignupComponent } from './components/signup/signup.component';
+import { DashboardComponent } from './components/dashboard/dashboard.component';
+import { RequestComponent } from './components/password-reset/request/request.component';
+import { ResponseComponent } from './components/password-reset/response/response.component';
+
 
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavBarComponent,
+    LoginComponent,
+    SignupComponent,
+    DashboardComponent,
+    RequestComponent,
+    ResponseComponent,
   ],
   imports: [
     BrowserModule
